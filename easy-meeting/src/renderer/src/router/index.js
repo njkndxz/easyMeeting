4a0e706367b5f423a9dd23f0d7a150dc5201a6d5
liuweijian <279423484@qq.com>
2025-08-05 11:50:47 +0800

    主路由修改

--- easy-meeting/src/renderer/src/router/index.js
+++ easy-meeting/src/renderer/src/router/index.js
@@ -14,7 +14,7 @@ const router = createRouter({
             path: "/",
             name: "home",
             component: () => import('@/views/Layout.vue'),
-            redirect: "/setting",
+            redirect: "/meetingMain",
             children: [
                 {
                     path: "/meetingMain",
